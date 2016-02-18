4b8da37e89f8fa4baeb9d8f3ddc17e945418605e
Blackburn29 <blackburn2929@gmail.com>
2016-02-18 13:59:43 -0600

    Add in missing files

---
+++ app/src/main/java/com/blakelafleur/pyli/ConnectionListAdapter.java
@@ -0,0 +1,59 @@
+package com.blakelafleur.pyli;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Blake on 2/17/2016.
+ */
+public class ConnectionListAdapter extends BaseAdapter {
+
+    private ArrayList<Connection> mConnections;
+    private LayoutInflater mInflater;
+
+    public ConnectionListAdapter(Context c, ArrayList<Connection> conn) {
+        this.mConnections = conn;
+        this.mInflater = LayoutInflater.from(c);
+    }
+
+    @Override
+    public int getCount() {
+        return this.mConnections.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return this.mConnections.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        TextView host;
+
+        if (null == view) {
+            view = this.mInflater.inflate(R.layout.connection_list_item, parent, false);
+            view.setTag(R.id.host, view.findViewById(R.id.host));
+        }
+
+        host = (TextView) view.getTag(R.id.host);
+        host.setText(mConnections.get(position).getHost());
+
+        return view;
+    }
+
+    public void setConnections(ArrayList<Connection> connections) {
+        this.mConnections = connections;
+    }
+}
---
+++ app/src/main/java/com/blakelafleur/pyli/OnConnectionAddedInterface.java
@@ -0,0 +1,11 @@
+package com.blakelafleur.pyli;
+
+/**
+ * Created by Blake on 2/17/2016.
+ */
+public interface OnConnectionAddedInterface {
+    /**
+     * A callback that is fired when a new connection is added to the database
+     **/
+    public void onConnectionAdded();
+}
